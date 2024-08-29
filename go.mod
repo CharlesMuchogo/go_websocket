b60e9f2a769ec0a392a9eef1be859a96c13f045b
Charles Muchogo <muchpaul2@gmail.com>
2024-08-29 13:08:10 +0300

    Added connection pool to broadcast message to the receiver and the sender only

--- go.mod
+++ go.mod
@@ -1,8 +1,8 @@
-module websocket
+module main.go
 
-go 1.19
+go 1.23.0
 
 require (
-	github.com/gorilla/mux v1.8.0 // indirect
-	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/gorilla/mux v1.8.1
+	github.com/gorilla/websocket v1.5.3
 )
--- main.go
+++ main.go
@@ -2,70 +2,103 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"net/http"
-	"time"
-
 	"github.com/gorilla/mux"
 	"github.com/gorilla/websocket"
+	"log"
+	"net/http"
+	"strconv"
+	"sync"
 )
 
 type message struct {
-    Message string `json:"message"`
-    Time    int64  `json:"time"`
+	Message    string `json:"text"`
+	Timestamp  uint64 `json:"timestamp"`
+	SenderId   uint64 `json:"senderId"`
+	ReceiverId uint64 `json:"receiverId"`
+	Read       bool   `json:"read"`
 }
 
 var (
 	wsUpgrader = websocket.Upgrader{
 		ReadBufferSize:  1024,
 		WriteBufferSize: 1024,
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
 	}
 
-	wsConn *websocket.Conn
+	connections = make(map[uint64]*websocket.Conn)
+	connMutex   sync.Mutex
 )
 
 func wsEndpoint(w http.ResponseWriter, r *http.Request) {
-	wsUpgrader.CheckOrigin = func(r *http.Request) bool {
-		return true
+	userIdStr := r.URL.Query().Get("userId")
+
+	if userIdStr == "" {
+		http.Error(w, "userId parameter is required", http.StatusBadRequest)
+		return
+	}
+
+	userId, err := strconv.ParseUint(userIdStr, 10, 64)
+
+	if err != nil {
+		http.Error(w, "invalid userId format", http.StatusBadRequest)
+		return
 	}
 
 	wsConn, err := wsUpgrader.Upgrade(w, r, nil)
-
 	if err != nil {
 		fmt.Printf("could not upgrade: %s \n", err.Error())
+		return
 	}
 	defer wsConn.Close()
 
-	for {
+	connMutex.Lock()
+	connections[userId] = wsConn
+	connMutex.Unlock()
 
-		currentTime := time.Now()
-        epoch := currentTime.Unix()
+	defer func() {
+		connMutex.Lock()
+		delete(connections, userId)
+		connMutex.Unlock()
+	}()
+
+	for {
 		var msg message
 		err := wsConn.ReadJSON(&msg)
-		msg.Time = epoch
-
-
 		if err != nil {
-			fmt.Printf("error reading json: %s \n", err.Error())			
-		}
-
-		fmt.Printf("message received: %s\n", msg.Message, )
-
-		// Echo the received message back to the client
-		err = wsConn.WriteJSON(msg)
-		if err != nil {
-			fmt.Printf("error writing json: %s \n", err.Error())
+			fmt.Printf("error reading json: %s \n", err.Error())
 			break
 		}
 
+		fmt.Printf("message received: %s\n", msg.Message)
+
+		broadcastMessage(msg)
+	}
+}
+
+func broadcastMessage(msg message) {
+	connMutex.Lock()
+	defer connMutex.Unlock()
+
+	if senderConn, ok := connections[msg.SenderId]; ok {
+		err := senderConn.WriteJSON(msg)
+		if err != nil {
+			fmt.Printf("error sending message to sender: %s \n", err.Error())
+		}
+	}
+
+	if receiverConn, ok := connections[msg.ReceiverId]; ok {
+		err := receiverConn.WriteJSON(msg)
+		if err != nil {
+			fmt.Printf("error sending message to receiver: %s \n", err.Error())
+		}
 	}
 }
 
 func main() {
-
 	router := mux.NewRouter()
 	router.HandleFunc("/", wsEndpoint)
 
 	log.Fatal(http.ListenAndServe(":9200", router))
-
 }
